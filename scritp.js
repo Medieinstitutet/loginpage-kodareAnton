d0a11b64511ab1b5f7b5fe24cec27a8635f83b66
Anton <94136841+kodareAnton@users.noreply.github.com>
2021-11-30 21:02:03 +0100

    klar

--- scritp.js
+++ scritp.js
@@ -1,37 +1,23 @@
 // gör en local storage med lagring av användarnamn
-localStorage.setItem("userName", "Janne");
-localStorage.setItem("userLname", "Kemi");
 
-let userName = localStorage.getItem("userName");
-let userLname = localStorage.getItem("userLname");
+let Name = localStorage.getItem("userName");
+let Lname = localStorage.getItem("userLname");
 
 // localStorage.clear();
 console.log(userName);
 console.log(userLname);
 
 
-// Ett click event med på logginknappen
-btn.addEventListener("click", function(){
-    let btn = document.getElementById("btn")
-    let inputOne = document.getElementById("inputone").Value;
-    let inputTwo = document.getElementById("inputtwo").Value;
 
-    let loggin = inputOne + inputTwo;
-    document.getElementById("text").innerHTML = "välkommen" + " " + inputOne + " " + inputTwo
-    
-    
-    console.log(loggin);
-});
-
-// Array
+// Array med alla 
 let ppl = [
     {
     Name: "Anton",
     Lname: "Tj",
 },
 {
-    Name: "Janne",
-    Lname: "Kemi",
+    Name: "janne",
+    Lname: "test",
 },
 {
     Name: "Adam",
@@ -46,7 +32,24 @@ let ppl = [
     Lname: "Ko",
 }];
 console.log(ppl);
-// lägg till i localstrage med "skapa konto knappen"
 
+
+// lägg till i localstrage med hjälp av JSON localStorage
+localStorage.setItem("ppl", JSON.stringify(ppl));
+let getPpl = JSON.parse(localStorage.getItem(ppl))
 
 // logga ut event 
+
+
+// Ett click event med på logginknappen
+btn.addEventListener("click", function(){
+    let btn = document.getElementById("btn")
+    let inputOne = document.getElementById("inputone").Value;
+    let inputTwo = document.getElementById("inputtwo").Value;
+
+    let loggin = inputOne + inputTwo;
+    document.getElementById("text").innerHTML = "välkommen" + " " + inputOne + " " + inputTwo
+    
+    
+    console.log(loggin);
+});
